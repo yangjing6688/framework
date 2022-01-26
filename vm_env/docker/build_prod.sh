4ef783a12721313f65675e419c696630f539dc92
elatour <elatour@extremenetworks.com>
2022-01-26 09:15:36 -0500

    -fixes

--- vm_env/docker/build_prod.sh
+++ vm_env/docker/build_prod.sh
@@ -2,8 +2,8 @@
 version=$(awk '/version_api/{print $NF}' version.json | sed 's/,//g' | sed 's/"//g')
 echo $version
 docker build  -t automation-dev-env . 
-docker tag automation-dev-env ghcr.io/extremenetworks/econ-automation-framework/automation-dev-env:latest
-docker tag automation-dev-env ghcr.io/extremenetworks/econ-automation-framework/automation-dev-env:$version
+docker tag automation-dev-env ghcr.io/extremenetworks/extreme_automation_framework/automation-dev-env:latest
+docker tag automation-dev-env ghcr.io/extremenetworks/extreme_automation_framework/automation-dev-env:$version
 echo $GIT_PAT | docker login ghcr.io --username=$GIT_USER  --password-stdin
-docker push ghcr.io/extremenetworks/econ-automation-framework/automation-dev-env:latest
-docker push ghcr.io/extremenetworks/econ-automation-framework/automation-dev-env:$version
+docker push ghcr.io/extremenetworks/extreme_automation_framework/automation-dev-env:latest
+docker push ghcr.io/extremenetworks/extreme_automation_framework/automation-dev-env:$version
