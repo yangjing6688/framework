c9a16e24ea5f452314447e8962df2b6bbbd3d4bc
elatour <elatour@extremenetworks.com>
2022-02-03 11:52:03 -0500

    aiq-1386

--- vm_env/upgrade/packages_upgrade_v017.sh
+++ vm_env/upgrade/packages_upgrade_v017.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 line=$(head -n 1 /automation/version.txt)
-if [ $line == "15.0.0" ]
+if [ $line == "17.0.0" ]
 then
-  
+   cd /automation/tests/extreme_automation_tests
+   pip install -r requirements.txt
+   
    echo "Update the VM Version"
-   sed -i 's/15.0.0/16.0.0/g' /automation/version.txt
+   sed -i 's/17.0.0/18.0.0/g' /automation/version.txt
    more /automation/version.txt 
    echo "Completed VM version update"
 else
