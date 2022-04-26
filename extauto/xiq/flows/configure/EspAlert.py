bf212ef4693e43db933db4ecf4a7e8a437e590f2
Zhihui(Tim) Chen) <zhchen19@outlook.com>
2022-04-26 17:08:19 +0800

    XIQ-4295 fix test case

--- extauto/xiq/flows/configure/EspAlert.py
+++ extauto/xiq/flows/configure/EspAlert.py
@@ -99,11 +99,15 @@ class EspAlert(EspAlertWebElements):
          - ``Find When In Configured Grid  ${when}``
         :return: returns 1 if successfully matched else -1
         """
-        for row in self.get_configured_grid_rows():
-            if self.get_when_in_rows(row).text == when:
-                return 1
-        self.screen.save_screen_shot()
-        return -1
+        if self.get_configured_grid_rows():
+            for row in self.get_configured_grid_rows():
+                if self.get_when_in_rows(row).text == when:
+                    return 1
+            self.screen.save_screen_shot()
+            return -1
+        else:
+            self.screen.save_screen_shot()
+            return -1
     def delete_alert_policy(self,when):
         """
         - Go to policy page and delete alert policy in configured grid
@@ -126,11 +130,11 @@ class EspAlert(EspAlertWebElements):
                     return 1
         self.screen.save_screen_shot()
         return -1
-    def search_in_unconfigure_grid(self,policy_type,search_input):
+    def search_in_unconfigure_grid(self,event_search_input,metric_search_input):
         """
         - Go to policy page and search in unconfigured grid
         - Keyword Usage
-         - ``Search In Unconfigure Grid  ${policy_type}  ${search_input}``
+         - ``Search In Unconfigure Grid  ${event_search_input}  ${metric_search_input}``
         :return: returns 1 if successfully search else -1
         """
         self.utils.switch_to_iframe(CloudDriver().cloud_driver)
@@ -140,13 +144,22 @@ class EspAlert(EspAlertWebElements):
         sleep(2)
         self.utils.print_info("Clicking Unconfigured Policies Tab")
         self.auto_actions.click(self.get_not_configred_tab_txt())
-        self.utils.print_info("Clicking policy type:" + policy_type)
-        self.auto_actions.click(getattr(self,"get_unconfigured_"+policy_type)())
+        self.utils.print_info("Clicking policy type: event")
+        self.auto_actions.click(self.get_unconfigured_event())
         self.utils.print_info("Clicking search icon")
         self.auto_actions.click(self.get_unconfigured_search_icon())
-        self.utils.print_info('Entering search :'+search_input)
-        self.auto_actions.send_keys(self.get_unconfigured_search_input(), search_input)
-        return self.find_desc_in_unconfigured_grid(search_input)
+        self.utils.print_info('Entering search :'+event_search_input)
+        self.auto_actions.send_keys(self.get_unconfigured_search_input(), event_search_input)
+        if self.find_desc_in_unconfigured_grid(event_search_input) == 1:
+            self.utils.print_info('Cleaning search')
+            self.auto_actions.send_keys(self.get_unconfigured_search_input(), "")
+            self.utils.print_info("Clicking policy type: metric")
+            self.auto_actions.click(self.get_unconfigured_metric())
+            self.utils.print_info('Entering search :'+metric_search_input)
+            self.auto_actions.send_keys(self.get_unconfigured_search_input(), metric_search_input)
+            return self.find_desc_in_unconfigured_grid(metric_search_input)
+        else:
+            return -1
     def find_desc_in_unconfigured_grid(self,desc):
         """
         - Go to policy page and find event/metric in unconfigured grid
@@ -173,9 +186,14 @@ class EspAlert(EspAlertWebElements):
         sleep(2)
         self.utils.print_info("Clicking Unconfigured Policies Tab")
         self.auto_actions.click(self.get_not_configred_tab_txt())
+        sleep(2)
+        self.utils.print_info("Matching:"+when)
         for row in self.get_unconfigured_grid_rows():
+            self.utils.print_info("Matching..."+self.get_desc_in_unconfigured_grid_rows(row).text)
             if self.get_desc_in_unconfigured_grid_rows(row).text == when:
-                self.auto_actions.click(self.get_add_icon_in_row(),row)
+                self.utils.print_info("successfully match with "+when)
+                self.auto_actions.click(self.get_add_icon_in_row(row))
+                sleep(2)
                 self.utils.print_info("Clicking trigger type:" + trigger_type)
                 self.auto_actions.click(getattr(self,"get_trigger_type_"+trigger_type)())
                 if policy_type == 'metric':
@@ -187,7 +205,12 @@ class EspAlert(EspAlertWebElements):
                 self.utils.print_info("Clicking save")
                 self.auto_actions.click(self.get_save())
                 sleep(5)
+                self.utils.print_info("Clicking Configured Policies Tab")
+                self.auto_actions.click(self.get_configred_tab_txt())
+                sleep(2)
                 return self.find_when_in_configured_grid(when)
+        self.utils.print_info("failed match with "+when)
+        self.screen.save_screen_shot()
         return -1
     def edit_alert_policy(self,when,severity,desc):
         """
@@ -201,9 +224,6 @@ class EspAlert(EspAlertWebElements):
         self.utils.print_info("Going to Policy page")
         self.auto_actions.click(self.get_go_to_policy())
         sleep(2)
-        self.utils.print_info("Opening profile dialog")
-        self.auto_actions.click(self.get_add_policy())
-        sleep(3)
         for row in self.get_configured_grid_rows():
             if self.get_when_in_rows(row).text == when:
                 self.auto_actions.click(self.get_when_in_rows(row))
@@ -211,8 +231,8 @@ class EspAlert(EspAlertWebElements):
                 self.utils.print_info("Clicking severity select:"+severity)
                 self.auto_actions.click(self.get_severity_select())
                 self.auto_actions.click(getattr(self,"get_severity_select_"+severity)())
-                self.utils.print_info("Entering desc text:"+desc)
-                self.auto_actions.send_keys(self.get_profile_description(),desc)
+                # self.utils.print_info("Entering desc text:"+desc)
+                # self.auto_actions.send_keys(self.get_profile_description(),desc)
                 self.utils.print_info("Clicking save")
                 self.auto_actions.click(self.get_save())
                 sleep(3)
