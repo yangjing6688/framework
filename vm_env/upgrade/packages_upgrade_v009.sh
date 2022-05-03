5829eb094213adfe7dae5597419649b3ea413eed
Peter Sadej <31295774+petersadej@users.noreply.github.com>
2022-05-03 14:32:22 -0400

    Remove pycurl install

--- vm_env/upgrade/packages_upgrade_v009.sh
+++ vm_env/upgrade/packages_upgrade_v009.sh
@@ -3,10 +3,6 @@
 line=$(head -n 1 /automation/version.txt)
 if [ $line == "8.0.0" ]
 then
-    echo "Install pycurl"
-    sudo apt install -y libcurl4-openssl-dev libssl-dev
-    echo "Completed Installing pycurl"
-
     echo "Update the VM Version"
     sed -i 's/8.0.0/9.0.0/g' /automation/version.txt
     more /automation/version.txt
