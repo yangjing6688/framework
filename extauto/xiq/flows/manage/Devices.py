5f2b17c6fe298c37c6e63f953d71c9c4bd2283bf
Philip Do <pdo@extremenetworks.com>
2022-06-06 13:52:43 -0700

    Update Devices.py

--- extauto/xiq/flows/manage/Devices.py
+++ extauto/xiq/flows/manage/Devices.py
@@ -9490,7 +9490,7 @@ class Devices:
         n_time = 0
         self.utils.print_info("Checking all device progress status ")
         while n_time < int(wait_time_in_min)*2:       # waits for 30s instead of 1 min before the next loop
-            self.utils.print_info("N times:  " + str(n_time))
+            self.utils.print_info("time has waited:  " + str(round(int(n_time)/2,2))) + " mins"
             rows = self.devices_web_elements.get_manage_all_devices_progress_status()
             if rows == None:
                break
@@ -9499,11 +9499,11 @@ class Devices:
                n_time = n_time + 1
                sleep(30)
 
-        self.utils.print_info("Out N times:  " + str(n_time))
+        self.utils.print_info("time completed waiting:  " + str(int(n_time)/2)) + " mins"
         if n_time > wait_time_in_min * 2:
             return -1
 
-        sleep(10)
+        sleep(30)
         self.utils.print_info("All devices finish updating ")
         return 1
 
