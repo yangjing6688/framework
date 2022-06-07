52460b7eabe2163b1ba8e9dbe45b39ae38f7c7e5
elatour <elatour@extremenetworks.com>
2022-06-07 15:12:01 -0400

    -Added a paging code for HIVE OS

--- ExtremeAutomation/Library/Device/Common/Agents/TelnetAgent.py
+++ ExtremeAutomation/Library/Device/Common/Agents/TelnetAgent.py
@@ -244,6 +244,8 @@ class TelnetAgent(CliAgent):
             self.debug_print(self.send_command("disable clipaging"))
         elif self.device.oper_sys == NetworkElementConstants.OS_VOSS:
             self.debug_print(self.send_command("terminal more disable"))
+        elif self.device.oper_sys == NetworkElementConstants.OS_HIVE:
+            self.debug_print(self.send_command("console page 0"))
         elif self.device.oper_sys in [NetworkElementConstants.OS_ALPHA,
                                       NetworkElementConstants.OS_BOSS,
                                       NetworkElementConstants.OS_SLX]:
