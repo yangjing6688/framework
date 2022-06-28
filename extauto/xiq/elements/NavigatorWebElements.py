d0b03611e3b505718995cb847fa22f09ac7f08eb
Hunise Ceetherakath <66333482+mceetherakath@users.noreply.github.com>
2022-06-29 00:25:47 +0530

    Keyword Modified - VPN Management
    VPN service is hidden under Manage sub-menu and enabled VPN Management page. So existing keyword 'Navigate to VPN Service Tab' is modified to 'Navigate To VPN Management Tab'

--- extauto/xiq/elements/NavigatorWebElements.py
+++ extauto/xiq/elements/NavigatorWebElements.py
@@ -662,8 +662,8 @@ class NavigatorWebElements(NavigatorWebElementDefinitions):
                 return order_number
         return -1
 
-    def get_vpn_services_tab(self):
-        return self.weh.get_element(self.vpn_services_tab)
+    def get_vpn_management_tab(self):
+        return self.weh.get_element(self.vpn_management_tab)
 
     def get_clients_hyperlink(self):
         return self.weh.get_element(self.clients_hyperlink)
