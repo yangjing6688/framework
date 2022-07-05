f90235cf982c7250af6c99f06e3f2125cb1b5176
abraitoru <popescua@extremenetworks.com>
2022-07-05 08:41:23 -0400

    Revert "ss"
    This reverts commit 9c4f9877677b1b608569c12e065bf7e214b9aebd.

--- extauto/xiq/defs/Device360WebElementDefs.py
+++ extauto/xiq/defs/Device360WebElementDefs.py
@@ -2206,7 +2206,6 @@ class Device360WebElementDefs:
     device360_thunderbold_icon_stack = \
         {
             'CSS_SELECTOR': '.power-supply',
-        }
 
     d360_create_port_type = \
         {
