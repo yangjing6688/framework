c308b6e9e4911b672db05bbe7d2c43b2d68855dd
wenqi-cao <wcao@extremenetworks.com>
2022-07-15 00:31:28 +0800

    Update DeviceConfigDefs.py

--- extauto/xiq/defs/DeviceConfigDefs.py
+++ extauto/xiq/defs/DeviceConfigDefs.py
@@ -580,9 +580,21 @@ class DeviceConfigDefs:
             'wait_for': 5
         }
 
+    device_config_audit_complete_view = \
+        {
+            'XPATH': '//a[contains(text(), "Complete")]',
+            'wait_for': 5
+        }
+
     device_config_audit_delta_view_content = \
         {
             'XPATH': '//div[@data-dojo-attach-point="deltaCtn"]',
+            'wait_for': 5
+        }
+
+    device_config_audit_complete_view_content = \
+        {
+            'XPATH': '//div[@data-dojo-attach-point="fullCtn"]',
             'wait_for': 5
         }
 
@@ -1289,7 +1301,26 @@ class DeviceConfigDefs:
 
     config_audit_delta_view_button = \
         {
+            'XPATH': '//span[@title="Configuration Audit"]',
+            'wait_for': 5
+        }
+
+    devices_config_audit_view_button = \
+        {
+            'XPATH': '//span[@title="Configuration Audit"]',
+            'wait_for': 5
+        }
+
+    devices_config_audit_mismatch = \
+        {
             'CSS_SELECTOR': '.ui-icon-sprite-mismatch',
+            'index': 0,
+            'wait_for': 5
+        }
+
+    devices_config_audit_match = \
+        {
+            'CSS_SELECTOR': '.ui-icon-sprite-match',
             'index': 0,
             'wait_for': 5
         }
@@ -1298,4 +1329,5 @@ class DeviceConfigDefs:
         {
             'XPATH': '//div[@data-dojo-attach-point="gridContent"]//table[@class="dgrid-row-table"]//td/..',
             'wait_for': 10
+        }
         
