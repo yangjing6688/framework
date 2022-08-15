c3bc7a23d1633be1698a4487806472d4bc0c791d
barbaras <bsochor@extremenetworks.com>
2022-08-14 22:24:48 -0700

    device_actions_reboot_menu_item updated

--- extauto/xiq/defs/DeviceActionsDefs.py
+++ extauto/xiq/defs/DeviceActionsDefs.py
@@ -6,14 +6,9 @@ class DeviceActionsDefs:
             'wait_for': 2
         }
 
-#    device_actions_reboot_menu_item = \
-#        {
-#            'XPATH': '//*[@data-automation-tag="automation-manage-device-actions-switch-reboot"]',
-#            'wait_for': 2
-#        }
-
     device_actions_reboot_menu_item = \
         {
+#            'XPATH': '//*[@data-automation-tag="automation-manage-device-actions-switch-reboot"]',
             'XPATH': '//*[@data-automation-tag="automation-manage-device-actions-ap-reboot"]',
             'wait_for': 2
         }
