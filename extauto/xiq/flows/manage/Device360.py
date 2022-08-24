f1d7a5f7819f722ae07ab889e82d222e6ec6cc2a
zoicanionut <dzoican@extremenetworks.com>
2022-08-24 10:28:16 -0400

    update_pse

--- extauto/xiq/flows/manage/Device360.py
+++ extauto/xiq/flows/manage/Device360.py
@@ -6352,24 +6352,8 @@ class Device360(Device360WebElements):
                 sleep(2)
                 get_pse_profile_items = self.get_select_element_port_type("pse_profile_items")
                 pse_profile_name = value['pse_profile_name']
-                self.utils.print_info("ITEM: ", value)
-                self.utils.print_info("PSE PROFILE NAME: ", pse_profile_name)
-                self.utils.print_info("PSE POWER LIMIT: ", value['pse_profile_power_limit'])
-                self.utils.print_info("PRIORITY: ", value['pse_profile_priority'])
-                self.utils.print_info("DESCRIPTION: ", value['pse_profile_description'])
 
-
-                pse_profile_found = False
-                self.utils.print_info(f"Searching for: {pse_profile_name} in the dropdown items...")
-
-                for pse_profile_item in get_pse_profile_items:
-                    if pse_profile_item.text == pse_profile_name:
-                        self.utils.print_info(f"Found PSE profile: {pse_profile_item.text} Selecting...")
-                        self.auto_actions.click(pse_profile_item)
-                        pse_profile_found = True
-
-                # if self.auto_actions.select_drop_down_options(get_pse_profile_items, pse_profile_name):
-                if pse_profile_found:
+                if self.auto_actions.select_drop_down_options(get_pse_profile_items, pse_profile_name):
                     self.utils.print_info(" Selected into dropdown value : ", pse_profile_name)
 
                     try:
