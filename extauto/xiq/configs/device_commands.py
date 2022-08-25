b0d3e2fd7cd97c8f130b9e5aaa88c69eae1e5066
wenqi-cao <wcao@extremenetworks.com>
2022-08-26 01:35:15 +0800

    Update device_commands.py

--- extauto/xiq/configs/device_commands.py
+++ extauto/xiq/configs/device_commands.py
@@ -1,7 +1,12 @@
-TURN_ON_OFF_WIFI_INTERFACE =      'networksetup -setairportpower '
+# MacOS commands
+MAC_GET_WIFI_INTERFACE_NAME    =      "networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}'"
+MAC_TURN_ON_OFF_WIFI_INTERFACE =      'networksetup -setairportpower '
+MAC_CHECK_WIFI_CONNECTION      =       'networksetup -getairportnetwork '
+MAC_SCAN_FOR_LIST_WIFI         =      '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan '
+MAC_CONNECT_TO_WIFI            =      'networksetup -setairportnetwork '
 IFCONFIG                   =      'ifconfig '
-SCAN_FOR_LIST_WIFI         =      '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan '
-CONNECT_TO_WIFI            =      'networksetup -setairportnetwork '
+
+# AP commands
 AP_CAPWAP_OFF              =      'no capwap client enable'
 AP_CAPWAP_ON               =      'capwap client enable'
 
