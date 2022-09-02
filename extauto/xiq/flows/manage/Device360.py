df8e4b1bacd8b846dc3b87c03eb42f017515bdcd
elatour <elatour@extremenetworks.com>
2022-09-02 11:27:35 -0400

    Updated the advanced onboard to look for the serial a couple of times

--- extauto/xiq/flows/manage/Device360.py
+++ extauto/xiq/flows/manage/Device360.py
@@ -371,8 +371,8 @@ class Device360(Device360WebElements):
             return -1
         else:
             self.utils.print_info(f"****************** Rerun the keyword device360_enable_ssh_cli_connectivity {retry_counter}")
-            self.device360_enable_ssh_cli_connectivity(device_mac, device_name, run_time, time_interval,
-                                                       retry_time, retry_counter, **kwargs)
+            return self.device360_enable_ssh_cli_connectivity(device_mac, device_name, run_time, time_interval,
+                                                              retry_time, retry_counter, **kwargs)
 
     def device360_enable_ssh_web_connectivity(self, device_mac='', device_name='', run_time=5):
         """
