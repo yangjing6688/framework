4c53eb58faa275a2d902bda5116ce2949ae81906
wenqi-cao <wcao@extremenetworks.com>
2022-09-09 23:28:31 +0800

    Update device_commands.py

--- extauto/xiq/configs/device_commands.py
+++ extauto/xiq/configs/device_commands.py
@@ -4,6 +4,7 @@ MAC_TURN_ON_OFF_WIFI_INTERFACE =      'networksetup -setairportpower '
 MAC_CHECK_WIFI_CONNECTION      =       'networksetup -getairportnetwork '
 MAC_SCAN_FOR_LIST_WIFI         =      '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan '
 MAC_CONNECT_TO_WIFI            =      'networksetup -setairportnetwork '
+MAC_IPV4_ADDRESS_FILTER                =      " | awk '/inet/{getline; print $2}'"
 IFCONFIG                   =      'ifconfig '
 
 # AP commands
