feb22e5bb293f09a4b1039c1675233597253664e
zoicanionut <dzoican@extremenetworks.com>
2022-09-15 08:54:18 -0400

    navigator fix

--- extauto/xiq/flows/common/Navigator.py
+++ extauto/xiq/flows/common/Navigator.py
@@ -201,7 +201,7 @@ class Navigator(NavigatorWebElements):
         self.navigate_to_configure_tab()
 
         self.utils.print_info("Selecting Common Objects")
-        self.auto_actions.click_referece(self.get_common_objects_sub_tab())
+        self.auto_actions.click_reference(self.get_common_objects_sub_tab())
         sleep(5)
 
     def navigate_to_network_policies_tab(self, **kwargs):
