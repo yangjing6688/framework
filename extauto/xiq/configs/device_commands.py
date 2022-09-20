547acfc98315f7a86df7849787f2ea4cbf3dfe82
wenqi-cao <wcao@extremenetworks.com>
2022-09-21 00:29:36 +0800

    Update device_commands.py

--- extauto/xiq/configs/device_commands.py
+++ extauto/xiq/configs/device_commands.py
@@ -1,13 +1,12 @@
 # MacOS commands
-MAC_GET_WIFI_INTERFACE_NAME    =      "networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}'"
-MAC_TURN_ON_OFF_WIFI_INTERFACE =      'networksetup -setairportpower '
-MAC_CHECK_WIFI_CONNECTION      =       'networksetup -getairportnetwork '
-MAC_SCAN_FOR_LIST_WIFI         =      '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan '
-MAC_CONNECT_TO_WIFI            =      'networksetup -setairportnetwork '
-MAC_IPV4_ADDRESS_FILTER                =      " | awk '/inet/{getline; print $2}'"
-IFCONFIG                   =      'ifconfig '
+MAC_GET_WIFI_INTERFACE_NAME = "networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}'"
+MAC_TURN_ON_OFF_WIFI_INTERFACE = 'networksetup -setairportpower '
+MAC_CHECK_WIFI_CONNECTION = 'networksetup -getairportnetwork '
+MAC_SCAN_FOR_LIST_WIFI = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan '
+MAC_CONNECT_TO_WIFI = 'networksetup -setairportnetwork '
+MAC_IPV4_ADDRESS_FILTER = " | awk '/inet/{getline; print $2}'"
+IFCONFIG = 'ifconfig '
 
 # AP commands
-AP_CAPWAP_OFF              =      'no capwap client enable'
-AP_CAPWAP_ON               =      'capwap client enable'
-
+AP_CAPWAP_OFF = 'no capwap client enable'
+AP_CAPWAP_ON = 'capwap client enable'
