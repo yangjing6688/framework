c9e8d75cb6d83e50ff72aee8f728c1805df48b51
zoicanionut <dzoican@extremenetworks.com>
2022-10-03 08:10:16 -0400

    ss

--- ExtremeAutomation/Imports/pytestExecutionHelper.py
+++ ExtremeAutomation/Imports/pytestExecutionHelper.py
@@ -13,24 +13,24 @@ class PytestExecutionHelper():
         self.logger = Logger()
 
         if defaultAction != "skip" and defaultAction != "fail":
-            fail("Unable to create the PytestExecutionHelper class, the defaultAction must be set to 'skip' or 'fail'. the value " + defaultAction + " is not valid")
+            fail(
+                "Unable to create the PytestExecutionHelper class, the defaultAction must be set to 'skip' or 'fail'. the value " + defaultAction + " is not valid")
 
         self.logger.log_info("PytestExecutionHelper: setting default action for test cases as: [" + defaultAction + "]")
         self.action = defaultAction
         self.setup_failure = False
-        
+
     def setSetupFailure(self, value):
         self.setup_failure = value
         self.__printException()
-         
+
     def testSkipCheck(self):
         if self.setup_failure:
             if self.action == "skip":
                 skip("Setup failed, so skipping the test")
             elif self.action == "fail":
                 fail("Setup failed, so failing the test")
-            
+
     def __printException(self):
         tb = traceback.format_exc()
         self.logger.log_error("Detected Exception: " + tb)
-        
