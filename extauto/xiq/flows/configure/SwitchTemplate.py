a9ed6ba0ccd3ca1b4f660050e3e08e6e3fa5886b
aldeabogdan <aldeab@extremenetworks.com>
2022-10-05 08:50:26 -0400

    Update SwitchTemplate.py

--- extauto/xiq/flows/configure/SwitchTemplate.py
+++ extauto/xiq/flows/configure/SwitchTemplate.py
@@ -2075,23 +2075,24 @@ class SwitchTemplate(object):
         return False
 
     def add_sw_template_from_policy_tab(self, sw_model, sw_template_name, save_template=True):
+        '''
+        This keyword add new template from policy tab
+        :param sw_model: model of template
+        :param sw_template_name: Name of template
+        :param save_template: True is template will be save; else False
+        :return: 1 if template has been created; else -1
+        '''
 
         if self.check_sw_template(sw_template_name):
             self.utils.print_info(
                 "Template with name {} already present in the template grid".format(sw_template_name))
             return -1
-
         add_btn = self.sw_template_web_elements.get_new_sw_template_add_button()
-        sleep(2)
-
         if add_btn:
             self.utils.print_info("Click on sw Template Add button")
             self.auto_actions.click(add_btn)
-            sleep(1)
-
             self.utils.print_info("select the sw: ", sw_model)
             sw_list_items = self.sw_template_web_elements.get_sw_template_platform_from_drop_down()
-            sleep(2)
             for el in sw_list_items:
                 self.utils.print_debug("Switch template names: ", el.text.upper())
                 if not el:
@@ -2100,23 +2101,18 @@ class SwitchTemplate(object):
                     self.auto_actions.click(el)
                     break
                 print(el.text)
-            sleep(3)
-
             self.utils.print_info("Enter the switch Template Name: ", sw_template_name)
             self.auto_actions.send_keys(self.sw_template_web_elements.get_sw_template_name_textfield(),
                                         sw_template_name)
-            sleep(3)
         if save_template:
-            sleep(5)
             save_btns = self.sw_template_web_elements.get_sw_template_save_button()
             for save_btn in save_btns:
                 if save_btn.is_displayed():
                     self.utils.print_info("Click on the save template button")
                     self.auto_actions.click(save_btn)
-                    sleep(10)
                     tool_tip_text = tool_tip.tool_tip_text
                     self.utils.print_info("Tool tip Text Displayed on Page", tool_tip_text)
-                    sleep(15)
+                    sleep(3)
                     for cnt3 in tool_tip_text:
                         if 'successfully' in cnt3:
                             self.utils.print_info("Found successfully message")
