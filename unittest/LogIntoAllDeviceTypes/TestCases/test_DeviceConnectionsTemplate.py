92d0d5383a2d19e47f66731ab6d557962b3f555d
elatour <elatour@extremenetworks.com>
2022-10-13 08:47:37 -0400

    fixed issue with checking for login prompt

--- unittest/LogIntoAllDeviceTypes/TestCases/test_DeviceConnectionsTemplate.py
+++ unittest/LogIntoAllDeviceTypes/TestCases/test_DeviceConnectionsTemplate.py
@@ -53,195 +53,181 @@ class DefaultTests:
         print(cmd_ret)
 
     @mark.p1  # Marked as a P1 test case
+    def test_01_log_all_generic_devices(self):
+        """ Log into All Device Types """
+        self.executionHelper.testSkipCheck()
+        self.defaultLibrary.apiUdks.setupTeardownUdks.Base_Test_Suite_Setup()
+        self.defaultLibrary.apiUdks.setupTeardownUdks.Base_Test_Suite_Cleanup()
+
+    @mark.p1  # Marked as a P1 test case
     def test_02_log_into_exos(self):
         """ Log into EXOS """
         self.executionHelper.testSkipCheck()
         # Login
-        self.defaultLibrary.deviceNetworkElement.networkElementConnectionManager.connect_to_network_element('test', '10.148.35.102', 'rwa', 'rwa', 'telnet', 'VOSS')
-        self.defaultLibrary.deviceNetworkElement.networkElementCliSend.send_cmd('test', 'show ?')
-        self.defaultLibrary.deviceNetworkElement.networkElementConnectionManager.connect_to_network_element('test',
-                                                                                                        '10.148.35.102',
-                                                                                                        'rwa', 'rwa',
-                                                                                                        'telnet',
-                                                                                                        'VOSS')
-        self.defaultLibrary.deviceNetworkElement.networkElementCliSend.send_cmd('test', 'show ?')
+        spawn = self.helper_login(self.tb.config.netelem1.ip,
+                                  self.tb.config.netelem1.port,
+                                  self.tb.config.netelem1.username,
+                                  self.tb.config.netelem1.password,
+                                  self.tb.config.netelem1.cli_type)
+        # Send good command
+        self.helper_send_command(spawn, 'show version')
+        # Send Bad command
+        self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
+        # Close spawn
+        self.cli.close_spawn(spawn)
 
-    # @mark.p1  # Marked as a P1 test case
-    # def test_01_log_all_generic_devices(self):
-    #     """ Log into All Device Types """
-    #     self.executionHelper.testSkipCheck()
-    #     self.defaultLibrary.apiUdks.setupTeardownUdks.Base_Test_Suite_Setup()
-    #     self.defaultLibrary.apiUdks.setupTeardownUdks.Base_Test_Suite_Cleanup()
-    #
-    # @mark.p1  # Marked as a P1 test case
-    # def test_02_log_into_exos(self):
-    #     """ Log into EXOS """
-    #     self.executionHelper.testSkipCheck()
-    #     # Login
-    #     spawn = self.helper_login(self.tb.config.netelem1.ip,
-    #                               self.tb.config.netelem1.port,
-    #                               self.tb.config.netelem1.username,
-    #                               self.tb.config.netelem1.password,
-    #                               self.tb.config.netelem1.cli_type)
-    #     # Send good command
-    #     self.helper_send_command(spawn, 'show version')
-    #     # Send Bad command
-    #     self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
-    #     # Close spawn
-    #     self.cli.close_spawn(spawn)
-    #
-    # @mark.p1  # Marked as a P1 test case
-    # def test_03_log_into_voss(self):
-    #     """ Log into VOSS """
-    #     self.executionHelper.testSkipCheck()
-    #     # Login
-    #     spawn = self.helper_login(self.tb.config.netelem2.ip,
-    #                               self.tb.config.netelem2.port,
-    #                               self.tb.config.netelem2.username,
-    #                               self.tb.config.netelem2.password,
-    #                               self.tb.config.netelem2.cli_type)
-    #     # Send good command
-    #     self.helper_send_command(spawn, 'show sys-info uboot')
-    #     # Send Bad command
-    #     self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
-    #     # Close spawn
-    #     self.cli.close_spawn(spawn)
-    #
-    # @mark.p1  # Marked as a P1 test case
-    # def test_04_log_into_wing_ap(self):
-    #     """ Log into Wing-ap """
-    #     self.executionHelper.testSkipCheck()
-    #     # Login
-    #     spawn = self.helper_login(self.tb.config.wing1.ip,
-    #                               self.tb.config.wing1.port,
-    #                               self.tb.config.wing1.username,
-    #                               self.tb.config.wing1.password,
-    #                               self.tb.config.wing1.cli_type)
-    #     # Send good command
-    #     self.helper_send_command(spawn, 'show boot')
-    #     # Send Bad command
-    #     self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
-    #     # Close spawn
-    #     self.cli.close_spawn(spawn)
-    #
-    # @mark.p1  # Marked as a P1 test case
-    # def test_05_log_into_sr_router_ap(self):
-    #     """ Log into sr router """
-    #     self.executionHelper.testSkipCheck()
-    #     # Login
-    #     spawn = self.helper_login(self.tb.config.aerohive_sw1.ip,
-    #                               self.tb.config.aerohive_sw1.port,
-    #                               self.tb.config.aerohive_sw1.username,
-    #                               self.tb.config.aerohive_sw1.password,
-    #                               self.tb.config.aerohive_sw1.cli_type)
-    #     # Send good command
-    #     self.helper_send_command(spawn, 'show sysinfo')
-    #     # Send Bad command
-    #     self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
-    #     # Close spawn
-    #     self.cli.close_spawn(spawn)
-    #
-    # @mark.p1  # Marked as a P1 test case
-    # def test_06_log_into_xr_router_ap(self):
-    #     """ Log into xr router """
-    #     self.executionHelper.testSkipCheck()
-    #     # Login
-    #     spawn = self.helper_login(self.tb.config.router1.ip,
-    #                               self.tb.config.router1.port,
-    #                               self.tb.config.router1.username,
-    #                               self.tb.config.router1.password,
-    #                               self.tb.config.router1.cli_type,
-    #                               connection_method = self.tb.config.router1.connection_method)
-    #     # Send good command
-    #     self.helper_send_command(spawn, 'show system disk-info')
-    #     # Send Bad command
-    #     self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
-    #     # Close spawn
-    #     self.cli.close_spawn(spawn)
-    #
-    #
-    # @mark.p1  # Marked as a P1 test case
-    # def test_07_log_into_ap(self):
-    #     """ Log into ap """
-    #     self.executionHelper.testSkipCheck()
-    #     # Login
-    #     spawn = self.helper_login(self.tb.config.ap1.ip,
-    #                               self.tb.config.ap1.port,
-    #                               self.tb.config.ap1.username,
-    #                               self.tb.config.ap1.password,
-    #                               self.tb.config.ap1.cli_type)
-    #     # Send good command
-    #     self.helper_send_command(spawn, 'show version')
-    #     # Send Bad command
-    #     self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
-    #     # Close spawn
-    #     self.cli.close_spawn(spawn)
-    #
-    # @mark.p1  # Marked as a P1 test case
-    # def test_08_log_into_win_mu(self):
-    #     """ Log into Win MU """
-    #     self.executionHelper.testSkipCheck()
-    #     # Login
-    #     spawn = self.helper_login(self.tb.config.mu1.ip,
-    #                               self.tb.config.mu1.port,
-    #                               self.tb.config.mu1.username,
-    #                               self.tb.config.mu1.password,
-    #                               self.tb.config.mu1.cli_type)
-    #     # Send good command
-    #     self.helper_send_command(spawn, 'dir')
-    #     # Send Bad command
-    #     self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
-    #     # Close spawn
-    #     self.cli.close_spawn(spawn)
-    #
-    # @mark.p1  # Marked as a P1 test case
-    # def test_09_log_into_mac_mu(self):
-    #     """ Log into MAC MU """
-    #     self.executionHelper.testSkipCheck()
-    #     # Login
-    #     spawn = self.helper_login(self.tb.config.mu2.ip,
-    #                               self.tb.config.mu2.port,
-    #                               self.tb.config.mu2.username,
-    #                               self.tb.config.mu2.password,
-    #                               self.tb.config.mu2.cli_type)
-    #     # Send good command
-    #     self.helper_send_command(spawn, 'ls -al')
-    #     # Send bad command
-    #     self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
-    #     # Close spawn
-    #     self.cli.close_spawn(spawn)
-    #
-    # @mark.p1  # Marked as a P1 test case
-    # def test_10_log_into_a3(self):
-    #     """ Log into A3 """
-    #     self.executionHelper.testSkipCheck()
-    #     # Login
-    #     spawn = self.helper_login(self.tb.config.a3.node1_ip,
-    #                               self.tb.config.a3.console_port,
-    #                               self.tb.config.a3.console_username,
-    #                               self.tb.config.a3.console_password,
-    #                               self.tb.config.a3.cli_type)
-    #     # Send good command
-    #     self.helper_send_command(spawn, 'ls -al')
-    #     # Send Bad command
-    #     self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
-    #     # Close spawn
-    #     self.cli.close_spawn(spawn)
-    #
-    # @mark.p1  # Marked as a P1 test case
-    # def test_11_log_into_linux(self):
-    #     """ Log into Linux MU """
-    #     self.executionHelper.testSkipCheck()
-    #     # Login
-    #     spawn = self.helper_login(self.tb.config.mu3.ip,
-    #                               self.tb.config.mu3.port,
-    #                               self.tb.config.mu3.username,
-    #                               self.tb.config.mu3.password,
-    #                               self.tb.config.mu3.cli_type)
-    #     # Send good command
-    #     self.helper_send_command(spawn, 'ls -al')
-    #     # Send bad command
-    #     self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
-    #     # Close spawn
-    #     self.cli.close_spawn(spawn)
+    @mark.p1  # Marked as a P1 test case
+    def test_03_log_into_voss(self):
+        """ Log into VOSS """
+        self.executionHelper.testSkipCheck()
+        # Login
+        spawn = self.helper_login(self.tb.config.netelem2.ip,
+                                  self.tb.config.netelem2.port,
+                                  self.tb.config.netelem2.username,
+                                  self.tb.config.netelem2.password,
+                                  self.tb.config.netelem2.cli_type)
+        # Send good command
+        self.helper_send_command(spawn, 'show sys-info uboot')
+        # Send Bad command
+        self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
+        # Close spawn
+        self.cli.close_spawn(spawn)
+
+    @mark.p1  # Marked as a P1 test case
+    def test_04_log_into_wing_ap(self):
+        """ Log into Wing-ap """
+        self.executionHelper.testSkipCheck()
+        # Login
+        spawn = self.helper_login(self.tb.config.wing1.ip,
+                                  self.tb.config.wing1.port,
+                                  self.tb.config.wing1.username,
+                                  self.tb.config.wing1.password,
+                                  self.tb.config.wing1.cli_type)
+        # Send good command
+        self.helper_send_command(spawn, 'show boot')
+        # Send Bad command
+        self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
+        # Close spawn
+        self.cli.close_spawn(spawn)
+
+    @mark.p1  # Marked as a P1 test case
+    def test_05_log_into_sr_router_ap(self):
+        """ Log into sr router """
+        self.executionHelper.testSkipCheck()
+        # Login
+        spawn = self.helper_login(self.tb.config.aerohive_sw1.ip,
+                                  self.tb.config.aerohive_sw1.port,
+                                  self.tb.config.aerohive_sw1.username,
+                                  self.tb.config.aerohive_sw1.password,
+                                  self.tb.config.aerohive_sw1.cli_type)
+        # Send good command
+        self.helper_send_command(spawn, 'show sysinfo')
+        # Send Bad command
+        self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
+        # Close spawn
+        self.cli.close_spawn(spawn)
+
+    @mark.p1  # Marked as a P1 test case
+    def test_06_log_into_xr_router_ap(self):
+        """ Log into xr router """
+        self.executionHelper.testSkipCheck()
+        # Login
+        spawn = self.helper_login(self.tb.config.router1.ip,
+                                  self.tb.config.router1.port,
+                                  self.tb.config.router1.username,
+                                  self.tb.config.router1.password,
+                                  self.tb.config.router1.cli_type,
+                                  connection_method = self.tb.config.router1.connection_method)
+        # Send good command
+        self.helper_send_command(spawn, 'show system disk-info')
+        # Send Bad command
+        self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
+        # Close spawn
+        self.cli.close_spawn(spawn)
+
+
+    @mark.p1  # Marked as a P1 test case
+    def test_07_log_into_ap(self):
+        """ Log into ap """
+        self.executionHelper.testSkipCheck()
+        # Login
+        spawn = self.helper_login(self.tb.config.ap1.ip,
+                                  self.tb.config.ap1.port,
+                                  self.tb.config.ap1.username,
+                                  self.tb.config.ap1.password,
+                                  self.tb.config.ap1.cli_type)
+        # Send good command
+        self.helper_send_command(spawn, 'show version')
+        # Send Bad command
+        self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
+        # Close spawn
+        self.cli.close_spawn(spawn)
+
+    @mark.p1  # Marked as a P1 test case
+    def test_08_log_into_win_mu(self):
+        """ Log into Win MU """
+        self.executionHelper.testSkipCheck()
+        # Login
+        spawn = self.helper_login(self.tb.config.mu1.ip,
+                                  self.tb.config.mu1.port,
+                                  self.tb.config.mu1.username,
+                                  self.tb.config.mu1.password,
+                                  self.tb.config.mu1.cli_type)
+        # Send good command
+        self.helper_send_command(spawn, 'dir')
+        # Send Bad command
+        self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
+        # Close spawn
+        self.cli.close_spawn(spawn)
+
+    @mark.p1  # Marked as a P1 test case
+    def test_09_log_into_mac_mu(self):
+        """ Log into MAC MU """
+        self.executionHelper.testSkipCheck()
+        # Login
+        spawn = self.helper_login(self.tb.config.mu2.ip,
+                                  self.tb.config.mu2.port,
+                                  self.tb.config.mu2.username,
+                                  self.tb.config.mu2.password,
+                                  self.tb.config.mu2.cli_type)
+        # Send good command
+        self.helper_send_command(spawn, 'ls -al')
+        # Send bad command
+        self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
+        # Close spawn
+        self.cli.close_spawn(spawn)
+
+    @mark.p1  # Marked as a P1 test case
+    def test_10_log_into_a3(self):
+        """ Log into A3 """
+        self.executionHelper.testSkipCheck()
+        # Login
+        spawn = self.helper_login(self.tb.config.a3.node1_ip,
+                                  self.tb.config.a3.console_port,
+                                  self.tb.config.a3.console_username,
+                                  self.tb.config.a3.console_password,
+                                  self.tb.config.a3.cli_type)
+        # Send good command
+        self.helper_send_command(spawn, 'ls -al')
+        # Send Bad command
+        self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
+        # Close spawn
+        self.cli.close_spawn(spawn)
+
+    @mark.p1  # Marked as a P1 test case
+    def test_11_log_into_linux(self):
+        """ Log into Linux MU """
+        self.executionHelper.testSkipCheck()
+        # Login
+        spawn = self.helper_login(self.tb.config.mu3.ip,
+                                  self.tb.config.mu3.port,
+                                  self.tb.config.mu3.username,
+                                  self.tb.config.mu3.password,
+                                  self.tb.config.mu3.cli_type)
+        # Send good command
+        self.helper_send_command(spawn, 'ls -al')
+        # Send bad command
+        self.helper_send_command(spawn, 'error commmand isssued', expect_error=True)
+        # Close spawn
+        self.cli.close_spawn(spawn)
 
