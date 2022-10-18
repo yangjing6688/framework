f9de8dd724237d558488f45e6673696fcabc25ce
Binhngx <bnguyen@extremenetworks.com>
2022-10-18 15:49:55 -0700

    Update

--- extauto/xiq/defs/DeviceConfigDefs.py
+++ extauto/xiq/defs/DeviceConfigDefs.py
@@ -191,7 +191,7 @@ class DeviceConfigDefs:
 
     device_configuration_dhcp_checkbox = \
         {
-            'XPATH': '//input[@data-dojo-attach-point="dhcpAddressOnly"]',
+            'XPATH': '//input[@data-dojo-attach-point="mgtd-dhcp"]',
             'wait_for': 5
         }
 
