d804902aa0e1db3893ad272ccf6663931e0690ca
shreenbanu <71691796+shreenbanu@users.noreply.github.com>
2022-10-20 13:00:24 +0530

    Update CommonObjects.py

--- extauto/xiq/flows/configure/CommonObjects.py
+++ extauto/xiq/flows/configure/CommonObjects.py
@@ -255,7 +255,6 @@ class CommonObjects(object):
             self.utils.print_info(f"Move to next page {page_counter+1}")
             self.auto_actions.click_reference(self.cobj_web_elements.get_next_page_element)
             sleep(5)
->>>>>>> main
 
         if not select_ssid_flag:
             kwargs['pass_msg'] = "Given SSIDs are not present. Nothing to delete!"
