6ec45309fbc214efbcf23a36b4f21ce928b868cc
amiinescu <mmiinescu@extremenetworks.com>
2022-11-10 03:19:49 -0500

    Added new modifications

--- extauto/xiq/flows/extreme_location/ExtremeLocation.py
+++ extauto/xiq/flows/extreme_location/ExtremeLocation.py
@@ -1553,7 +1553,7 @@ class ExtremeLocation(ExtremeLocationWebElements):
                 self.utils.print_info(f'Device Classification Rule Created successfully with {user_type} '
                                       f'with Duration {expected_duration}')
                 kwargs['pass_msg'] = f'Device Classification Rule Created successfully with {user_type} '
-                                      f'with Duration {expected_duration}'
+                                     f'with Duration {expected_duration}'
                 self.common_validation.passed(**kwargs)
                 return 1
             else:
