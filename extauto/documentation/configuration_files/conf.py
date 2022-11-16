a665e5453a2a2c633b10d8c8781439646c5b6904
Peter Sadej <31295774+petersadej@users.noreply.github.com>
2022-11-16 08:49:54 -0500

    Update docs generation

--- extauto/documentation/configuration_files/conf.py
+++ extauto/documentation/configuration_files/conf.py
@@ -10,23 +10,17 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/home/rvellasamy/extreme_automation_framework'))
-sys.path.insert(0, os.path.abspath('/home/rvellasamy/extreme_automation_framework/extauto'))
-sys.path.insert(0, os.path.abspath('/home/rvellasamy/extreme_automation_framework/extauto/xiqse'))
-#sys.path.insert(0, os.path.abspath('/home/automation/cw_automation'))
-sys.path.insert(0, os.path.abspath('../../extreme_automation_framework/'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', '..')))
 
 source_suffix = ['.rst', '.md']
 
 # -- Project information -----------------------------------------------------
 
-project = 'AutoIQ'
+project = 'Extreme Automation Framework'
 copyright = '2022, Extreme Networks'
 author = 'Extreme Networks'
 
@@ -40,7 +34,8 @@ release = 'Main'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'sphinx_markdown_builder','sphinx.ext.autodoc'
+        # 'sphinx_markdown_builder',
+        'sphinx.ext.autodoc'
 ]
 
 
@@ -58,9 +53,11 @@ exclude_patterns = ['build/*','_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_mock_imports = ['cv2']
