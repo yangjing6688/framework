e764034e412f0ffd0fb30b79a75ca2800921b07c
elatour <elatour@extremenetworks.com>
2022-11-30 11:29:17 -0500

    added login xapi keyword

--- extauto/xiq/xapi/common/XapiLogin.py
+++ extauto/xiq/xapi/common/XapiLogin.py
@@ -1,25 +1,36 @@
 from pprint import pprint
 import time
+from extauto.xiq.xapi.XapiHelper import XapiHelper
+
 try:
-    import extremecloudiq2
+    import extremecloudiq
     from extremecloudiq.rest import ApiException
 except:
     pprint('WARNING: The library for ExtremecloudIQ cannot be loaded, please ensure this libaray is installed if you are trying to use the XAPI')
 
 
-class Example:
+class XapiLogin:
 
-    def login(self, url, username, password):
+    def __init__(self):
+        self.xapiHelper = XapiHelper()
+
+    def login(self, username, password, **kwargs):
         """
            - Login to the XIQ instance and return the configuration that can be used to send other commands.
 
            :param url - The XIQ Instance url
            :param username - The username for the XIQ instance
            :param password - The password for the XIQ instance
-           :return: api_response  This will be None if the command failed or will contain the JSON from the API call
+           :return: 1 if success or -1 if failure
        """
+        return_code = -1
+
+        # get the xapi URL from the global variables
+        xapi_url = self.xapiHelper.get_xapi_url()
+
+        # FIXME - Make the token longer [24 hours]
         configuration = extremecloudiq.Configuration(
-            host=url,
+            host=xapi_url,
             username=username,
             password=password
         )
@@ -35,35 +46,11 @@ class Example:
                 api_response = api_instance.login(xiq_login_request)
                 pprint(api_response)
                 configuration.access_token = api_response.access_token
+                self.xapiHelper.set_xapi_configuration(configuration)
+                self.xapiHelper.common_validation.passed(**kwargs)
+                return 1
             except ApiException as e:
                 print("Exception when calling AuthenticationApi->login: %s\n" % e)
-                raise e
 
-        return configuration
-
-
-    def get_viq_info(self, configuration):
-        """
-            - Get the VIQ information
-
-            :param configuration: The configuration that was returned from the login.
-            :return: api_response  This will be None if the command failed or will contain the JSON from the API call
-        """
-
-        api_response = None
-        # Check that the access_token is in
-        if configuration.access_token == None:
-            raise "Error: access_token is None in the configuration"
-
-        # Enter a context with an instance of the API client
-        with extremecloudiq.ApiClient(configuration) as api_client:
-            # Create an instance of the API class
-            api_instance = extremecloudiq.AccountApi(api_client)
-            try:
-                # Get VIQ Info
-                api_response = api_instance.get_viq_info()
-                pprint(api_response)
-            except ApiException as e:
-                print("Exception when calling AccountApi->get_viq_info: %s\n" % e)
-                raise e
-        return api_response
+        self.xapiHelper.common_validation.failed(**kwargs)
+        return -1
---
+++ extauto/xiq/xapi/globalsettings/XapiGlobalSettings.py
@@ -0,0 +1,45 @@
+from pprint import pprint
+import time
+from extauto.xiq.xapi.XapiHelper import XapiHelper
+
+try:
+    import extremecloudiq2
+    from extremecloudiq.rest import ApiException
+except:
+    pprint('WARNING: The library for ExtremecloudIQ cannot be loaded, please ensure this libaray is installed if you are trying to use the XAPI')
+
+
+class XapiGlobalSettings:
+
+    def __init__(self):
+        self.xapiHelper = XapiHelper()
+        self.common_validation = CommonValidation()
+
+    def get_viq_info(self, **kwargs):
+        """
+            - Get the VIQ information
+
+            :param configuration: The configuration that was returned from the login.
+            :return: api_response  This will be None if the command failed or will contain the JSON from the API call
+        """
+
+        # Get the configuration from the Global varibles
+
+
+        api_response = None
+        # Check that the access_token is in
+        if configuration.access_token == None:
+            raise "Error: access_token is None in the configuration"
+
+        # Enter a context with an instance of the API client
+        with extremecloudiq.ApiClient(configuration) as api_client:
+            # Create an instance of the API class
+            api_instance = extremecloudiq.AccountApi(api_client)
+            try:
+                # Get VIQ Info
+                api_response = api_instance.get_viq_info()
+                pprint(api_response)
+            except ApiException as e:
+                print("Exception when calling AccountApi->get_viq_info: %s\n" % e)
+                raise e
+        return api_response
