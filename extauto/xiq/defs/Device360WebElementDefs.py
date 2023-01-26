3b4c3baf4bc19a7a147cfdcba96b49741d08f0ec
sorinampopa1 <sopopa@extremenetworks.com>
2023-01-26 10:41:34 -0500

    commit

--- extauto/xiq/defs/Device360WebElementDefs.py
+++ extauto/xiq/defs/Device360WebElementDefs.py
@@ -3305,8 +3305,8 @@ class Device360WebElementDefs:
 
     select_element_port_type_pse_profile_power_limit = \
         {
-            'XPATH': '//div[@data-dojo-attach-point="vlanObjForm"]//input[@data-dojo-attach-point="powerLimit"]',
-            'wait_for': 5
+	          'XPATH': '//div[@data-dojo-attach-point="vlanObjForm"]//input[@data-dojo-attach-point="powerLimit"]',
+	          'wait_for': 5
         }
 
     select_element_port_type_pse_profile_power_mode_dropdown = \
