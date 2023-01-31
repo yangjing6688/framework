723d39cd5a6cfe0b67537ec58f2dd4fc3afcd107
rvellasamy <65337702+rvellasamy@users.noreply.github.com>
2023-01-31 12:17:11 +0530

    EXA-323
    Verified and Handled Unknown Error Message in Manage--> Devices Page

--- extauto/xiq/flows/common/Navigator.py
+++ extauto/xiq/flows/common/Navigator.py
@@ -145,6 +145,11 @@ class Navigator(NavigatorWebElements):
         """
         if self.get_devices_page():
             self.utils.print_info("Already in Devices page")
+            self.utils.print_info("Checking for Unknown Tooltip Error message after login..")
+            if self.get_unknown_tooltip_error().is_displayed():
+                self.screen.save_screen_shot()
+                self.utils.print_info("Found Unknown Tooltip Error In Devices Page.So Closing the Error Message")
+                self.auto_actions.click_reference(self.get_unknown_error_tooltip_close_icon)
             sleep(10)
             self.enable_page_size(page_size='100')
             return 1
@@ -157,6 +162,11 @@ class Navigator(NavigatorWebElements):
                     self.enable_page_size(page_size='100')
                     kwargs['pass_msg'] = "Navigation Successful to Devices Sub tab on Monitor Tab"
                     self.common_validation.passed(**kwargs)
+                    self.utils.print_info("Checking for Unknown Tooltip Error message after login..")
+                    if self.get_unknown_tooltip_error().is_displayed():
+                       self.screen.save_screen_shot()
+                       self.utils.print_info("Found Unknown Tooltip Error In Devices Page.So Closing the Error Message")
+                       self.auto_actions.click_reference(self.get_unknown_error_tooltip_close_icon)
                     return 1
                 else:
                     self.utils.print_info("Unable to navigate to Devices tab")
