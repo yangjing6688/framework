18da426080e13e45619fd9cf9b498281eda7ac80
amiinescu <mmiinescu@extremenetworks.com>
2023-02-07 10:57:47 -0500

    Added modification to check_app_iqagent

--- ExtremeAutomation/Apis/NetworkElement/GeneratedApis/ParseApis/CLI/hostinformation/VOSS/base/baseversion/baseunit/HostinformationCustomShowTools.py
+++ ExtremeAutomation/Apis/NetworkElement/GeneratedApis/ParseApis/CLI/hostinformation/VOSS/base/baseversion/baseunit/HostinformationCustomShowTools.py
@@ -38,7 +38,8 @@ class HostinformationCustomShowTools(HostinformationBaseCustomShowTools):
         return result, {"ret_prompt_name": prompt_name}
 
     def check_app_iqagent(self, output, args, **kwargs):
-        parse_result = self.pw.get_value_by_offset(output, "Agent Version", 5)
+        output = output.replace("\n", "\r\n")
+        parse_result = self.pw.get_value_by_offset(output, "Agent Version", 3)
 
         result = True if args["iqagent_version"] == parse_result else False
         return result, {"ret_iqagent_version": parse_result}
