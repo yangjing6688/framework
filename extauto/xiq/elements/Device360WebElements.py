e53f6d081fbf8e0002104a4c75e216318c2087bb
administrator <icosmineanu@extremenetworks.com>
2023-02-23 11:01:01 -0500

    commit 45707 changes

--- extauto/xiq/elements/Device360WebElements.py
+++ extauto/xiq/elements/Device360WebElements.py
@@ -1842,27 +1842,27 @@ class Device360WebElements(Device360WebElementDefs):
         table_rows[0].location_once_scrolled_into_view
         return [
             row for row in table_rows if not
-            any(field in row.text for field in ["PORT NAME", "LLDP NEIGHBOR", "PORT STATUS", "Type", "ACCES VLAN"])
+            any(field in row.text for field in ["PORT NAME", "LLDP NEIGHBOR", "PORT STATUS"])
         ]
 
     def get_device360_ports_table_pagination_sizes(self):
         return self.weh.get_elements(self.device360_ports_table_pagination_sizes)
 
-
-    def get_device360_ports_table(self):
-        header_row = self.get_device360_ports_description_table_row()
-        ths = self.weh.get_elements(self.device360_ports_table_th_columns, parent=header_row)
-
-        table_rows = self.get_device360_port_table_rows()[1:]
-        results = []
-        for row in table_rows:
-            result = {}
-            tds = self.weh.get_elements(self.device360_ports_table_td_gridcell, parent=row)
-            for th, td in zip(ths, tds):
-                if th.text.strip():
-                    result[th.text.strip()] = td.text.strip()
-            results.append(result)
-        return results
+    #
+    # def get_device360_ports_table(self):
+    #     header_row = self.get_device360_ports_description_table_row()
+    #     ths = self.weh.get_elements(self.device360_ports_table_th_columns, parent=header_row)
+    #
+    #     table_rows = self.get_device360_port_table_rows()[1:]
+    #     results = []
+    #     for row in table_rows:
+    #         result = {}
+    #         tds = self.weh.get_elements(self.device360_ports_table_td_gridcell, parent=row)
+    #         for th, td in zip(ths, tds):
+    #             if th.text.strip():
+    #                 result[th.text.strip()] = td.text.strip()
+    #         results.append(result)
+    #     return results
 
     def get_device360_ah_icon(self, index):
         return self.weh.get_template_element(self.device360_ah_icons, index=index)
@@ -1934,20 +1934,20 @@ class Device360WebElements(Device360WebElementDefs):
         return {th.text.strip(): th for th in ths if th.text.strip()}
 
     # There is a duplicate of this function above that was commented out on 1/18/23
-    # def get_device360_ports_table(self):
-    #     header_row = self.get_device360_ports_description_table_row()
-    #     ths = self.weh.get_elements(self.device360_ports_table_th_columns, parent=header_row)
-    #
-    #     table_rows = self.get_device360_port_table_rows()
-    #     results = []
-    #     for row in table_rows:
-    #         result = {}
-    #         tds = self.weh.get_elements(self.device360_ports_table_td_gridcell, parent=row)
-    #         for th, td in zip(ths, tds):
-    #             if th.text.strip():
-    #                 result[th.text.strip()] = td.text.strip()
-    #         results.append(result)
-    #     return results
+    def get_device360_ports_table(self):
+        header_row = self.get_device360_ports_description_table_row()
+        ths = self.weh.get_elements(self.device360_ports_table_th_columns, parent=header_row)
+
+        table_rows = self.get_device360_port_table_rows()
+        results = []
+        for row in table_rows:
+            result = {}
+            tds = self.weh.get_elements(self.device360_ports_table_td_gridcell, parent=row)
+            for th, td in zip(ths, tds):
+                if th.text.strip():
+                    result[th.text.strip()] = td.text.strip()
+            results.append(result)
+        return results
 
     def get_device360_pagination_page_buttons(self):
         return self.weh.get_elements(self.d360_pagination_page_button)
