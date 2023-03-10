197fc1cb249a4def7895a8954b09c28e3e7b18c3
Sergiu Ghita <sghita@extremenetworks.com>
2023-03-10 13:06:08 -0500

    AIQ-2764 Add Util pytest integration test

---
+++ ExtremeAutomation/IntegrationTests/extauto/common/utils/pytest/TUT.py
@@ -0,0 +1,31 @@
+from extauto.common.Utils import Utils
+import sys
+import pytest
+
+_path = '/automation/framework/extreme_automation_framework/ExtremeAutomation/IntegrationTests/extauto/common/utils/pytest/TUT.py'
+
+@pytest.fixture(scope="module", autouse=True)
+def my_fixture():
+    if __file__ == _path:
+        raise RuntimeError("File must be copy over")
+
+def test_python_builtin_import():
+    module = sys.modules.get("robot.libraries.BuiltIn")
+    with pytest.raises(AttributeError):
+        getattr(module.BuiltIn, "should_not_be_true")
+    getattr(module.BuiltIn, "isRunningPytest")
+
+
+def test_print_info():
+    utils = Utils()
+    utils.print_info("message_print_info")
+
+
+def test_print_warning():
+    utils = Utils()
+    utils.print_warning("message_print_warning")
+
+
+def test_print_error():
+    utils = Utils()
+    utils.print_error("message_print_error")
