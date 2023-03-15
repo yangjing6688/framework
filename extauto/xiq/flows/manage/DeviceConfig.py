6aea92946e6a0bf0fa0d5c79b75084ff34e75e82
Andreea Poschina <aposchina@extremenetworks.com>
2023-03-15 10:25:40 -0400

    XIQ-1053  (#958)
    * - modified click into click_reference
    - removed duplicate keywords: configure_port_name_usage_tab and close_port_type_config
    
    * - small adjustments on Device360 class
    
    * modified Save Port Configuration button XPATH
    
    * commit
    
    * removed unused imports
    
    * removed one unnecessary f string
    
    * modified failure messages to be alligned with main
    
    ---------
    
    Co-authored-by: vstefan <vstefan@extremenetworks.com>
    Co-authored-by: Eric Latour <39134465+elatour2000@users.noreply.github.com>

--- extauto/xiq/flows/manage/DeviceConfig.py
+++ extauto/xiq/flows/manage/DeviceConfig.py
@@ -17,6 +17,7 @@ from extauto.xiq.flows.common.DeviceCommon import DeviceCommon
 from extauto.xiq.flows.manage.Tools import Tools
 from extauto.xiq.flows.manage.Devices import Devices
 
+
 class DeviceConfig(DeviceConfigElements):
     def __init__(self):
         super().__init__()
@@ -3237,6 +3238,8 @@ class DeviceConfig(DeviceConfigElements):
 
         for _ in range(retries):
             try:
+                self.devices.refresh_devices_page()
+                self.utils.wait_till(timeout=5)
                 self.devices.select_device(device_mac=dut.mac)
 
                 btn, _ = self.utils.wait_till(
@@ -3250,7 +3253,7 @@ class DeviceConfig(DeviceConfigElements):
                 self.utils.print_info("Successfully got the device_config_audit_view button")
 
                 res, _ = self.utils.wait_till(
-                    func=lambda: self.auto_actions.click(btn),
+                    func=lambda: self.auto_actions.click_reference(self.get_device_config_audit_view),
                     delay=4,
                     exp_func_resp=True,
                     silent_failure=True
@@ -3270,7 +3273,7 @@ class DeviceConfig(DeviceConfigElements):
                 self.utils.print_info("Successfully got the delta_view button")
 
                 res, _ = self.utils.wait_till(
-                    func=lambda: self.auto_actions.click(delta_view),
+                    func=lambda: self.auto_actions.click_reference(self.get_device_config_audit_delta_view),
                     timeout=60,
                     delay=20,
                     exp_func_resp=True,
@@ -3297,7 +3300,8 @@ class DeviceConfig(DeviceConfigElements):
 
                 for command in commands:
 
-                    assert re.search(command, delta_configs), f"Did not find this command in delta CLI: {command}"
+                    assert re.search(command, delta_configs), f"Did not find this command in delta CLI: {command}, " \
+                                                              f"found {delta_configs} instead"
                     self.utils.print_info(f"Successfully found this command in delta CLI: {command}")
 
             except Exception as exc:
@@ -3320,13 +3324,14 @@ class DeviceConfig(DeviceConfigElements):
                     )
 
                     self.utils.wait_till(
-                        func=lambda: self.auto_actions.click(close_btn) == 1,
+                        func=lambda:
+                        self.auto_actions.click_reference(self.get_device_config_audit_view_close_button) == 1,
                         exp_func_resp=True,
                         delay=4,
                         silent_failure=True
                     )
-                except Exception:
-                    pass
+                except Exception as e:
+                    self.utils.print_info(repr(e))
 
                 self.devices.select_device(device_mac=dut.mac)
         else:
