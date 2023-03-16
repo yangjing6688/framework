31b1280beee8fe94f9a551766bc556b1f67e7885
John Subler <jsubler@extremenetworks.com>
2023-03-16 09:33:15 -0400

    Updated change_device_management_settings

--- extauto/xiq/elements/GlobalSettingWebElements.py
+++ extauto/xiq/elements/GlobalSettingWebElements.py
@@ -241,11 +241,11 @@ class GlobalSettingWebElements(GlobalSettingWebElementDefinitions):
     def get_ssh_availability_option_status(self):
         return self.weh.get_element(self.ssh_availability_option_status)
 
-    def get_exos_device_management_settings_status(self):
-        return self.weh.get_element(self.exos_device_management_settings_status)
+    def get_device_management_settings_status(self):
+        return self.weh.get_element(self.device_management_settings_status)
 
-    def get_exos_device_management_settings_save_button(self):
-        return self.weh.get_element(self.exos_device_management_settings_save_button)
+    def get_device_management_settings_save_button(self):
+        return self.weh.get_element(self.device_management_settings_save_button)
 
     def get_enable_copilot_feature_option_status(self):
         return self.weh.get_element(self.enable_copilot_feature_option_status)
